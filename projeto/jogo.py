c795fbec0cbb32ebeae7e9eabcfc2fe0313bf935
Luiz Karpinski <euluizkarpinski@gmail.com>
2025-07-01 17:15:05 -0300

    feat(game): turn-based combat created

--- projeto/jogo.py
+++ projeto/jogo.py
@@ -14,7 +14,17 @@ class Personagem:
     return self.__nivel
   
   def exibir_detalhes(self):
-    return f"Nome: {self.get_nome()} \nVida: {self.get_vida()} \nNivel: {self.get_nivel()}"
+    return f"\nNome: {self.get_nome()} \nVida: {self.get_vida()} \nNivel: {self.get_nivel()}"
+  
+  def receber_ataque(self, dano):
+    self.__vida -= dano
+    if self.__vida < 0:
+      self.__vida = 0
+
+  def atacar(self, alvo):
+    dano = self.__nivel * 2
+    alvo.receber_ataque(dano)
+    print(f"{self.get_nome()} deu {dano} de dano!")
   
 class Heroi(Personagem):
   def __init__(self, nome, vida, nivel, habilidade):
@@ -26,6 +36,11 @@ class Heroi(Personagem):
   
   def exibir_detalhes(self):
     return f"{super().exibir_detalhes()} \nHailidade: {self.get_habilidade()}"
+  
+  def especial(self, alvo):
+    dano = self.get_nivel() * 5
+    alvo.receber_ataque(dano)
+    print(f"{self.get_nome()} deu {dano} de dano com seu especial com {self.get_habilidade()}!")
     
 class Inimigo(Personagem):
   def __init__(self, nome, vida, nivel, tipo):
@@ -38,7 +53,37 @@ class Inimigo(Personagem):
   def exibir_detalhes(self):
     return f"{super().exibir_detalhes()} \nTipo: {self.get_tipo()}"
 
-heroi = Heroi(nome="Eduarrrdoo", vida=100, nivel=13, habilidade="Xaveco")
-print(heroi.exibir_detalhes())
-inimigo = Inimigo(nome="Laura", vida=100, nivel=13, tipo="ignorante")
-print(inimigo.exibir_detalhes())
+class Jogo:
+  def __init__(self) -> None:
+    self.heroi = Heroi(nome="Eduarrrdoo", vida=100, nivel=49, habilidade="Xaveco")
+    self.inimigo = Inimigo(nome="Laura", vida=100, nivel=7, tipo="ignorante")
+
+  def iniciar_batalha(self):
+    print("Iniciando Batalha")
+
+    while self.heroi.get_vida() > 0 and self.inimigo.get_vida() > 0:
+      print("\nDetalhes dos Personagens")
+      print(self.heroi.exibir_detalhes())
+      print(self.inimigo.exibir_detalhes())
+
+      input("Pressione Enter para atacar...")
+
+      escolha = input("1 - Ataque normal \n2 - Ataque especial: ")
+      if (escolha == "1"):
+        self.heroi.atacar(self.inimigo)
+      elif (escolha == "2"):
+        self.heroi.especial(self.inimigo)
+      else:
+        print("escolha invalida")
+
+      if self.inimigo.get_vida() > 0:
+        self.inimigo.atacar(self.heroi)
+    
+    if self.heroi.get_vida() > 0:
+      print("Win!")
+    else:
+      print("Voce perdeu!")
+
+
+jogo = Jogo()
+jogo.iniciar_batalha()
